63c2564027451b36cdca47cd6ba9063c50f3825f
facu <facu11ja@gmail.com>
2022-06-26 07:12:20 -0300

    se agrega la clase formacion

---
+++ portfolio1/Models/Formacion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace portfolio1.Models
+{
+    public class Formacion
+    {
+        private string _titulo;
+        private int _año;
+        private string _institucion;
+        private string _desc;
+        public Formacion(string titulo, int año, string institucion, string desc)
+        {
+            _titulo = titulo;
+            _año = año;
+            _institucion = institucion;
+            _desc = desc;
+        }
+
+        public string Institucion
+        {
+            get { return _institucion; }
+            set { _institucion = value; }
+        }
+        public string Desc
+        {
+            get { return _desc; }
+            set { _desc = value; }
+        }
+        public int Año
+        {
+            get { return _año; }
+            set { _año = value; }
+        }
+
+        public string Titulo
+        {
+            get { return _titulo; }
+            set { _titulo = value; }
+        }
+
+    }
+}
