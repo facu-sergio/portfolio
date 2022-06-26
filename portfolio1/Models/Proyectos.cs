f3b0d88b39d4b61f1a7b50872fbc59eca3160f46
facu <facu11ja@gmail.com>
2022-06-26 07:28:44 -0300

    se agrega la clase proyectos y redes

---
+++ portfolio1/Models/Proyectos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace portfolio1.Models
+{
+    public class Proyectos
+    {
+        private string _nombre;
+        private string _link;
+        private string _descripcion;
+        public Proyectos(string nombre, string link, string descripcion)
+        {
+            _nombre = nombre;
+            _link = link;
+            _descripcion = descripcion;
+        }
+
+        public string Descripcion
+        {
+            get { return _descripcion; }
+            set { _descripcion = value; }
+        }
+
+
+        public string Link
+        {
+            get { return _link; }
+            set { _link = value; }
+        }
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+
+    }
+}
