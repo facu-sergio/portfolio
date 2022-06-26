58eb634256285f9c53735e608e5232d332115505
facu <facu11ja@gmail.com>
2022-06-26 07:05:13 -0300

    creación del repositorio y primera subida de archivos con clases

---
+++ portfolio1/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace portfolio1.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ portfolio1/Models/Persona.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace portfolio1.Models
+{
+    public class Persona
+    {
+        private int _dni;
+        private string _nombre;
+        private string _apellido;
+
+        public Persona(int dni, string nombre, string apellido)
+        {
+            this._dni = dni;
+            this._nombre = nombre;
+            this._apellido = apellido;
+        }
+
+        public string apellido
+        {
+            get { return _apellido; }
+            set { _apellido = value; }
+        }
+
+
+        public string nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+
+        public int dni
+        {
+            get { return _dni; }
+            set { _dni = value; }
+        }
+
+
+
+    }
+}
