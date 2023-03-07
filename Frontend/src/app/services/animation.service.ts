1d1349e60bb46f11d46a52ed8657876b08d8e356
facu <facu11ja@gmail.com>
2023-03-07 05:49:26 -0300

    Fix animacion 2

--- Frontend/src/app/services/animation.service.ts
+++ Frontend/src/app/services/animation.service.ts
@@ -10,18 +10,18 @@ export class AnimationService {
   cargarImagen =  (entradas:any,observer:IntersectionObserver)=>{
     entradas.forEach((entrada:any) => {
       if(entrada.isIntersecting){
-        //entrada.target.classList.remove('invisible-izq')
+        entrada.target.classList.remove('invisible-izq')
         entrada.target.classList.add('visible');
         
       }else{
-        //entrada.target.classList.add('invisible-izq')
+        entrada.target.classList.add('invisible-izq')
         entrada.target.classList.remove('visible')
       }
     });
   }
   observer =  new IntersectionObserver(this.cargarImagen,{
     root: null,
-    rootMargin: '50px 50px 50px 50px',
-    threshold: 0.7
+    rootMargin: '0px',
+    threshold: 0.25,
   });
 }
